91ef15a186a362d3647258e8152bca8ea049b4db
Hosseinreza <hrakbari77@gmail.com>
2023-06-08 00:28:20 +0330

    Ad console.logs

--- app/server.js
+++ app/server.js
@@ -7,6 +7,8 @@ run();
 async function run (){
 
     await sock.bind("tcp://127.0.0.1:7000");
+    console.log("Server is ready listening on port 7000");
+    console.log("Press any key to start sending the jobs!");
     
 
 
