105f1a91cb33a0b79cfa4af838233bb17a477bc3
Hosseinreza <hrakbari77@gmail.com>
2023-06-14 00:51:10 +0330

    Add console for connection

--- app/worker.js
+++ app/worker.js
@@ -5,7 +5,7 @@ run();
 
 async function run(){
     await sock.connect("tcp://127.0.0.1:7000");
-
+    console.log("Connected to server.")
     for await( const msg of sock){
         console.log(`received job ${msg.toString}`);
     }
