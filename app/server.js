266807064142a913d50f03a5640751b46a83e034
Hosseinreza <hrakbari77@gmail.com>
2023-06-14 00:52:28 +0330

    process

--- app/server.js
+++ app/server.js
@@ -9,7 +9,7 @@ async function run (){
     await sock.bind("tcp://127.0.0.1:7000");
     console.log("Server is ready listening on port 7000");
     console.log("Press any key to start sending the jobs!");
-    Process.stdin.once("data",send);
+    process.stdin.once("data",send);
 
 
 }
